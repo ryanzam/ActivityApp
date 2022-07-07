39eeb0a46e770e1fceb8a72ccfca583bab795677
Ranjam al <ranjam.alee@ineo.fi>
2022-07-07 21:03:11 +0300

    add activitiy date and toaster

--- ActivityApp/Models/CreateProposalModel.cs
+++ ActivityApp/Models/CreateProposalModel.cs
@@ -2,18 +2,21 @@
 
 namespace ActivityApp.Models
 {
-	public class CreateProposalModel
-	{
-		[Required]
-		[MinLength(15)]
-		[MaxLength(50)]
-		public string Title { get; set; }
-		[Required]
-		[MinLength(1)]
-		[Display(Name = "Activity")]
-		public string ActivityId { get; set; }
-		[Required]
-		[MaxLength(200)]
-		public string Description { get; set; }
-	}
+    public class CreateProposalModel
+    {
+        [Required]
+        [MinLength(15)]
+        [MaxLength(50)]
+        public string Title { get; set; }
+        [Required]
+        [MinLength(1)]
+        [Display(Name = "Activity")]
+        public string ActivityId { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime ActivityDate { get; set; } = DateTime.Now;
+    }
 }
